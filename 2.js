6f5d6eb5f48be091ebf651e4a9be7e8d68e22618
TheRayCannon <100610106+TheRayCannon@users.noreply.github.com>
2022-03-21 15:03:26 -0600

    Update 2.js

--- 2.js
+++ 2.js
@@ -10,7 +10,8 @@ function getAlphaNumericAlphabet(){
   const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
   let alphaNumericAlphabet
 
-  // Your code here
+  alphabet.unshift(1, 2, 3, 4, 5, 6, 7, 8, 9,)
+  alphabet.push("&")
 
   return alphaNumericAlphabet
 }
